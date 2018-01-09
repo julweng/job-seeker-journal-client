3c5a40730487000c2730e8d4fcf75c0754ead487
julweng <julweng@gmail.com>
2018-01-09 15:52:17 -0800

    edit dashboardHeader and test

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import UserNav from '../components/userNav/userNav';
+import Header from '../components/dashboardHeader/header';
 
 export default function Dashboard(Props) {
   return (
     <div>
       <UserNav />
       <main role="main">
+        <Header />
       </main>
       <footer>Built by Julia Weng</footer>
     </div>
---
+++ src/components/dashboardHeader/header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './header.css';
+
+export default function Header(props) {
+  return (
+    <header role="banner">
+      <h1>Profile Summary</h1>
+    </header>
+  );
+}
